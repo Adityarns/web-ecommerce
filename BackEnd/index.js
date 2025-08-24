e1f3a5c2d41a54d24833f947c6e6574792c84162
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-08-24 21:08:47 +0700

    Menambahkan menu menu dari folder crud

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,3 +9,10 @@ mongoose.connect("mongodb://localhost:27017/Kedai_kopi");
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Database connected.."));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+// jalankan server
+app.listen(5000, () => console.log("Server up and running"));
