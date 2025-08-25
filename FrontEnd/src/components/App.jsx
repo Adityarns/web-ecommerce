ee903fe6063aa07307aa9cf36a0d5cb078bfb160
Aditya Rahman Syach <adityarachmansyach@gmail.com>
2025-08-25 20:47:44 +0700

    mengubah warna bg dan text pada navbar

--- FrontEnd/src/components/App.jsx
+++ FrontEnd/src/components/App.jsx
@@ -1,11 +1,40 @@
-import Navbar from "./Navbar";
-
-function App() {
+export default function App() {
   return (
-    <div className="flex text-4xl">
-      <Navbar></Navbar>
+    <div className="justify-center text-center  sm:pt-16">
+      <div className="hero grid lg:grid-cols-2 grid-cols-1 py-10 md:py-36 items-center xl:gap-0 gap-5 px-6 max-w-7xl mx-auto animate__animated animate__fadeInUp animate__delay-0.5s">
+        <div className="flex flex-col items-center lg:items-start justify-center text-center md:text-left lg:pl-6 xl:pl-30">
+          <div>
+            <h1 className="text-6xl/snug font-semibold text-[#589507] italic">
+              Matcha Moments
+            </h1>
+            <p className="py-3 text-2xl">
+              Dari ritual tradisional Jepang hingga cangkir Anda, rasakan
+              kelezatan matcha autentik setiap hari
+            </p>
+          </div>
+          <div className="flex flex-wrap justify-center md:justify-start gap-4">
+            <a
+              href="#about"
+              className="py-3 px-6 md:py-4 md:px-10 bg-[#144e94] hover:bg-[#146C94] rounded-3xl text-white"
+            >
+              About Me
+            </a>
+            <a
+              href="#project"
+              className="py-3 px-6 md:py-4 md:px-10 bg-[#0A0d14] hover:bg-[#146C94] rounded-3xl text-white border-[#144e94] border-2"
+            >
+              My Projects
+            </a>
+          </div>
+        </div>
+
+        <img
+          // src={DataImage.HeroImage}
+          alt="Hero Image"
+          className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] lg:w-[350px] lg:h-[350px] rounded-full object-cover mx-auto border-[#144e94] border-3"
+          loading="lazy"
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import DataImage from "../data";
+// import DataImage from "../data";
 
 export default function Navbar() {
   const underlineClass =
-    "md:absolute md:left-0 md:-bottom-1 md:w-full md:h-[2px] md:bg-[#144e94] md:scale-x-0 md:group-hover:scale-x-75 md:transition-transform md:origin-center md:duration-200";
+    "md:absolute md:left-0 md:-bottom-1 md:w-full md:h-[2px] md:bg-[#748E63] md:scale-x-0 md:group-hover:scale-x-75 md:transition-transform md:origin-center md:duration-200";
   const [active, setActive] = useState(false);
 
   useEffect(() => {
@@ -17,16 +17,16 @@ export default function Navbar() {
   return (
     <div
       className={`
-        navbar w-full z-50 px-10 flex justify-between items-center
-        py-6 md:py-4 text-amber-50 transition-all duration-300
+        navbar w-full z-50  flex justify-around items-center
+        py-6  text-[#589507] transition-all duration-300
         md:fixed md:top-0 md:left-0 
-        md:bg-[#0A0d14]/80 md:backdrop-blur-md md:shadow-md
+        md:bg-[#FAF8ED]/80 md:backdrop-blur-md md:shadow-md
       `}
     >
       <div className="logo flex">
-        <img src={DataImage.LogoImage} alt="LogoImage" className="w-[50px]" />
-        <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">
-          Portfolio
+        {/* <img src={DataImage.LogoImage} alt="LogoImage" className="w-[100px]" /> */}
+        <h1 className="flex text-4xl font-bold bg-white text-black p-1 md:bg-transparent md:text-[#589507] items-center">
+          MyMatcha
         </h1>
       </div>
 
@@ -45,7 +45,7 @@ export default function Navbar() {
         <li>
           <a
             href="#home"
-            className="flex relative group text-4xl font-medium md:hover:text-[#144e94]"
+            className="flex relative group text-xl font-bold md:hover:text-[#748E63]"
           >
             Home
             <span className={underlineClass}></span>
@@ -54,7 +54,7 @@ export default function Navbar() {
         <li>
           <a
             href="#about"
-            className="relative group text-lg font-medium md:hover:text-[#144e94]"
+            className="flex relative group text-xl font-bold md:hover:text-[#748E63]"
           >
             About
             <span className={underlineClass}></span>
@@ -63,40 +63,19 @@ export default function Navbar() {
         <li>
           <a
             href="#project"
-            className="relative group text-lg font-medium md:hover:text-[#144e94]"
+            className="flex relative group text-xl font-bold md:hover:text-[#748E63]"
           >
-            Project
+            Menu
             <span className={underlineClass}></span>
           </a>
         </li>
         <li>
           <a
             href="#contact"
-            className="relative group text-lg font-medium md:hover:text-[#144e94]"
+            className="flex relative group text-xl font-bold md:hover:text-[#748E63]"
           >
             Contact
             <span className={underlineClass}></span>
-          </a>
-        </li>
-      </ul>
-
-      <ul className="flex gap-3 color-">
-        <li>
-          <a href="https://github.com/Adityarns">
-            <img
-              // src={DataImage.GithubImage}
-              alt="GithubImage"
-              className="w-[30px]"
-            />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/aditya-rahman-syach-b9096133a/">
-            <img
-              // src={DataImage.LinkedinImage}
-              alt="LinkedinImage"
-              className="w-[35px]"
-            />
           </a>
         </li>
       </ul>
